cfd3ecfe827803cecff8bedea26d5088104bd510
Patryk Godlewski <pgodlewski099@gmail.com>
2022-07-04 11:47:31 +0200

    badge and heroCard update

--- components/atoms/Badge/Badge.tsx
+++ components/atoms/Badge/Badge.tsx
@@ -5,18 +5,23 @@ type PropTypes = {
   type: 'white' | 'black' | 'outlined-white' | 'outlined-black';
 };
 
-const styles = 'p-2 px-4 rounded uppercase font-bold';
+const styles =
+  'p-2 px-4 rounded uppercase font-bold max-w-min whitespace-nowrap';
 
 const span = ({ text, type }: PropTypes) => {
   if (type === 'white') {
-    return <div className={`${styles} bg-gray-50`}>{text}</div>;
+    return <div className={`${styles} text-gray-900  bg-gray-50`}>{text}</div>;
   }
 
   if (type === 'black') {
-    return (
-      <div className={`p-2 px-4 rounded bg-gray-50 uppercase`}>{text}</div>
-    );
+    return <div className={`${styles} text-gray-50 bg-gray-900 `}>{text}</div>;
   }
+
+  return (
+    <div className={`${styles} border dark:border-gray-50 border-gray-900 `}>
+      {text}
+    </div>
+  );
 };
 
 export default span;
--- components/molecules/HeroCard/HeroCard.tsx
+++ components/molecules/HeroCard/HeroCard.tsx
@@ -1,3 +1,4 @@
+import Badge from '@components/atoms/Badge/Badge';
 import Button from '@components/atoms/Button/Button';
 import React from 'react';
 
@@ -19,11 +20,13 @@ const HeroCard = ({ className, title, desc, badge, index }: any): any => {
         from-white to-transparent z-10`}
         />
       </div>
-      <div className="absolute inset-0 flex flex-col justify-end p-4 gap-4 z-10">
-        {/* <Badge>{badge}</Badge> */}
-        <h2>{title}</h2>
-        <p className="font-thin">{desc}</p>
-        <Button type="invertedFull">{`Graj teraz w ${title}`}</Button>
+      <div className="absolute inset-0 flex flex-col justify-between p-4 gap-4 z-10">
+        <Badge text={badge} type="white" />
+        <div className="flex flex-col gap-4">
+          <h2>{title}</h2>
+          <p className="font-thin">{desc}</p>
+          <Button type="invertedFull">{`Graj teraz w ${title}`}</Button>
+        </div>
       </div>
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent opacity-50 rounded" />
       <img
